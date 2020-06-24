877be82698f8178f80f328b0dfd11c539539cc4a
Vasily Fomin <vasilyf@amazon.com>
2020-06-24 15:52:37 -0700

    Latest changes.

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-ln -s $(pwd)/.vimrc $HOME/.vimrc
-ln -s $(pwd)/.tmux.conf $HOME/.tmux.conf
+set -e
+
+ln --symbolic --force --no-dereference $(pwd)/.config/i3 ${HOME}/.config/
+ln --symbolic --force --no-dereference $(pwd)/.config/i3status ${HOME}/.config/
+ln --symbolic --force --no-dereference $(pwd)/.config/nvim ${HOME}/.config/
+ln --symbolic --force --no-dereference $(pwd)/.config/wallpapers ${HOME}/.config/
+ln --symbolic --force --no-dereference $(pwd)/.config/compton.conf ${HOME}/.config/
+ln --symbolic --force --no-dereference $(pwd)/.vimrc ${HOME}/.vimrc
+ln --symbolic --force --no-dereference $(pwd)/.tmux.conf ${HOME}/.tmux.conf
+ln --symbolic --force --no-dereference $(pwd)/.XResources ${HOME}/
